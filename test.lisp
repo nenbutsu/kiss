8f8ed6d564f2795929056543f06b69f4d3a79132
nenbutsu <bmonkey@nifty.com>
2017-06-26 14:24:13 +0900

    report unhandled errors

--- test.lisp
+++ test.lisp
@@ -4,19 +4,26 @@
 
 (defglobal test-files
   '("test_cons.lisp"
-    "test_control.lisp"))
+    "test_control.lisp"
+    "test_predicate.lisp"))
 
-(while test-files
-  (let* ((name (car test-files))
-	 (file (open-input-file name))
+(defun test-file (name)
+  (let* ((file (open-input-file name))
 	 (form nil))
     (format (standard-output) "~%Testing ~S~%" name)
     (setq form (read file nil 'eof))
-    (while (and (not (eq form 'eof)) (eval form))
-      ;;(format (standard-output) "~S => t~%" form)
+    (while (not (eq form 'eof))
+      (with-handler (lambda (c)
+		      (format (standard-output) "Unhandled error signaled: ~S~%" form)
+		      (signal-condition c nil))
+		    (eval form))
       (setq form (read file nil 'eof)))
     (if (not (eq form 'eof))
-	(format (standard-output) "NIL returned. test: ~S" form))
+	(format (standard-output) "NIL returned. test: ~S~%" form))
     (format (standard-output) "OK~%"))
+  nil)
+
+(while test-files
+  (test-file (car test-files))
   (setq test-files (cdr test-files)))
 
