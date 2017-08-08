16e01ceef7ce5802f37aadddd0551a8abe8e6cfc
nenbutsu <bmonkey@nifty.com>
2017-08-08 16:17:56 +0900

    added more test forms

--- test_stream.lisp
+++ test_stream.lisp
@@ -18,6 +18,7 @@
 ;;; streamp
 (eq (streamp (standard-input)) t)
 (eq (streamp '()) nil)
+(eq (streamp "love") nil)
 
 ;;; input-stream-p
 (eq (input-stream-p (standard-input)) t)
@@ -29,9 +30,13 @@
 (eq (output-stream-p (standard-input)) nil)
 (eq (output-stream-p "hello") nil)
 
+;; error-output
+(eq (output-stream-p (error-output)) t)
+
 ;; with-standard-input
-(with-standard-input (create-string-input-stream "this is a string")
-	(list (read) (read)))
+(equal (with-standard-input (create-string-input-stream "this is a string")
+			    (list (read) (read)))
+       '(this is))
 
 ;; with-standard-output
 (let ((message nil))
@@ -46,6 +51,58 @@
 	(format (error-output) "love me tender...")
 	(setq message (get-output-stream-string (error-output))))
   (string= message "love me tender..."))
+
+;; with-open-output-file, with-open-input-file
+(null (with-open-output-file (outstream "example.dat")
+			     (format outstream "hello")))
+(eq (with-open-input-file (instream "example.dat")
+			  (read instream))
+    'hello)
+
+;; create-string-input-stream
+(equal (let ((str (create-string-input-stream "this is a string")))
+	 (list (read str) (read str) (read str)))
+       '(this is a))
+
+;; create-string-output-stream
+(equal (let ((str (create-string-output-stream)))
+	 (format str "hello")
+	 (format str "world")
+	 (get-output-stream-string str))
+       "helloworld")
+
+;; get-output-stream-string stream
+(equal (let ((out-str (create-string-output-stream)))
+	 (format out-str "This is a string")
+	 (let ((part1 (get-output-stream-string out-str)))
+	   (format out-str "right!")
+	   (list part1 (get-output-stream-string out-str))))
+       '("This is a string" "right!"))
+
+;; read
+(defglobal str (create-string-input-stream "hello #(1 2 3) 123 #\\A"))
+(eq (read str) 'hello)
+(equal (read str) #(1 2 3))
+(eql (read str) 123)
+(eql (read str) #\A)
+
+;; read-char
+(defglobal str (create-string-input-stream "hi"))
+(eql (read-char str) #\h)
+(eql (read-char str) #\i)
+(block a
+  (with-handler (lambda (condition)
+		  (if (instancep condition (class <error>))
+		      (return-from a t)
+		    (signal-condition condition nil)))
+		(read-char str))
+  nil)
+
+;; preview-char
+(equal (let ((s (create-string-input-stream "foo")))
+	 (list (preview-char s) (read-char s) (read-char s)))
+       '(#\f #\f #\o))
+
 
 ;; read-line
 (null (with-open-output-file (out "newfile")
