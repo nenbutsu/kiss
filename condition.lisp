68be32fef751dd7a659c034772b5a51abd00c2f3
nenbutsu <bmonkey@nifty.com>
2017-09-29 22:40:17 +0900

    ILOS refinement

--- condition.lisp
+++ condition.lisp
@@ -17,6 +17,108 @@
 
 (defdynamic *kiss::handlers* nil)
 
+;;  <object>
+;;     |
+;;     +--> <serious-condition>
+;;              |
+;;              +--> <error>
+;;              |       |
+;;              |       +--> <arithmetic-error>
+;;              |       |           |
+;;              |       |           +--> <division-by-zero>
+;;              |       |           +--> <floating-point-overflow>
+;;              |       |           +--> <floating-point-underflow>
+;;              |       |
+;;              |       +--> <control-error>
+;;              |       +--> <parse-error>
+;;              |       +--> <program-error>
+;;              |       |           |
+;;              |       |           +--> <domain-error>
+;;              |       |           +--> <undefined-entity>
+;;              |       |                     |
+;;              |       |                     +--> <unbound-variable>
+;;              |       |                     +--> <undefined-function>
+;;              |       +--> <simple-error>
+;;              |       +--> <stream-error>
+;;              |                   |
+;;              |                   +--> <end-of-stream>
+;;              +--> <storage-exhausted>
+;;      
+
+
+(defclass <serious-condition> (<object>)
+  ((continuable :reader condition-continuable :initarg continuable
+                :writer set-condition-continuable :initform 'nil))
+  (:metaclass <built-in-class>)
+  (:abstractp t))
+
+(defclass <storage-exhausted> (<serious-condition>) ()
+  (:metaclass <built-in-class>))
+
+(defclass <error> (<serious-condition>)
+  ((id :reader error-id :initarg error-id :initform 'nil))
+  (:metaclass <built-in-class>)
+  (:abstractp t))
+
+(defclass <simple-error> (<error>)
+  ((format-string :reader simple-error-format-string :initarg format-string
+                  :initform "simple error")
+   (format-arguments :reader simple-error-format-arguments
+                     :initarg format-arguments
+                     :initform nil))
+  (:metaclass <built-in-class>))
+
+(defclass <stream-error> (<error>)
+  ((stream :reader stream-error-stream :initarg stream))
+  (:metaclass <built-in-class>))
+
+(defclass <end-of-stream> (<stream-error>) ()
+  (:metaclass <built-in-class>))
+
+(defclass <arithmetic-error> (<error>)
+  ((operation :reader arithmetic-error-operation :initarg operation)
+   (operands :reader arithmetic-error-operands :initarg operands))
+  (:metaclass <built-in-class>))
+
+(defclass <division-by-zero> (<arithmetic-error>) ()
+  (:metaclass <built-in-class>))
+
+(defclass <floating-point-overflow> (<arithmetic-error>) ()
+  (:metaclass <built-in-class>))
+
+(defclass <floating-point-underflow> (<arithmetic-error>) ()
+  (:metaclass <built-in-class>))
+
+(defclass <control-error> (<error>)
+  ((name :reader control-error-name :initarg name)
+   (namespace :reader control-error-namespace :initarg namespace))
+  (:metaclass <built-in-class>))
+
+(defclass <parse-error> (<error>)
+  ((string :reader parse-error-string :initarg string)
+   (expected-class :reader parse-error-expected-class :initarg expected-class))
+  (:metaclass <built-in-class>))
+
+(defclass <program-error> (<error>) ()
+  (:metaclass <built-in-class>))
+
+(defclass <domain-error> (<program-error>)
+  ((object :reader domain-error-object :initarg object)
+   (expected-class :reader domain-error-expected-class :initarg expected-class))
+  (:metaclass <built-in-class>))
+
+(defclass <undefined-entity> (<program-error>)
+  ((name :reader undefined-entity-name :initarg name)
+   (namespace :reader undefined-entity-namespace :initarg namespace))
+  (:metaclass <built-in-class>))
+
+(defclass <unbound-variable> (<undefined-entity>) ()
+  (:metaclass <built-in-class>))
+
+(defclass <undefined-function> (<undefined-entity>) ()
+  (:metaclass <built-in-class>))
+
+
 ;; special operator: (IGNORE-ERRORS FORM*) -> <object>
 ;;   Establishes a handler for <error>, such that if an error occurs during
 ;;   execution of FORMs, ignore-errors will immediately return nil. Then it
