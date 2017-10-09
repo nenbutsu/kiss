2c9d1a524a8ac9ad43462f0e32b43a4f76d4f4d9
Yuji Minejima <bmonkey@nifty.com>
2017-10-08 17:36:25 -0700

    moved to stream.c

--- load.c
+++ load.c
@@ -18,14 +18,3 @@
 */
 #include "kiss.h"
 
-kiss_obj* kiss_load(const kiss_obj* const filename) {
-     size_t saved_heap_top = Kiss_Heap_Top;
-     kiss_obj* in = kiss_open_input_file(filename, KISS_NIL);
-     kiss_obj* form = kiss_c_read(in, KISS_NIL, KISS_EOS);
-     while (form != KISS_EOS) {
-	  kiss_eval(form);
-	  form = kiss_c_read(in, KISS_NIL, KISS_EOS);
-     }
-     Kiss_Heap_Top = saved_heap_top;
-     return KISS_T;
-}
