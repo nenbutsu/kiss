542dae4013a880eb39856db08efc5a145aeea14f
Yuji Minejima <bmonkey@nifty.com>
2017-10-31 16:47:57 -0700

    Added test forms for read function

--- test/test.lisp
+++ test/test.lisp
@@ -3,15 +3,16 @@
 ;;
 
 (defglobal test-files
-  '("test/test_symbol.lisp"
-    "test/test_cons.lisp"
-    "test/test_control.lisp"
-    "test/test_predicate.lisp"
-    "test/test_string.lisp"
-    "test/test_sequence.lisp"
-    "test/test_array.lisp"
-    "test/test_number.lisp"
-    "test/test_stream.lisp"))
+    '("test/test_read.lisp"
+      "test/test_symbol.lisp"
+      "test/test_cons.lisp"
+      "test/test_control.lisp"
+      "test/test_predicate.lisp"
+      "test/test_string.lisp"
+      "test/test_sequence.lisp"
+      "test/test_array.lisp"
+      "test/test_number.lisp"
+      "test/test_stream.lisp"))
 
 (defun test-file (name)
   (let* ((file (open-input-file name))
