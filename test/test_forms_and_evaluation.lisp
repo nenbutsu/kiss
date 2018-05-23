b38878c06bccbe18ec6cfd45cb7ad31676286c76
Yuji Minejima <yuji@minejima.jp>
2018-05-24 04:14:40 +0900

    adding tests

--- test/test_forms_and_evaluation.lisp
+++ test/test_forms_and_evaluation.lisp
@@ -1,7 +1,7 @@
 ;;; -*- mode: lisp; coding: utf-8 -*- 
 ;;; test_forms_and_evaluation.lisp --- a bunch of forms with which ISLisp processor must return true.
 
-;; Copyright (C) 2017 Yuji Minejima.
+;; Copyright (C) 2017, 2018 Yuji Minejima <yuji@minejima.jp>
 
 ;; This file is part of ISLisp processor KISS.
 
@@ -14,4 +14,58 @@
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
+
+;;; functionp
+(functionp (function car))
+(functionp (function create))
+(functionp (lambda () t))
+(not (functionp 'car))
+
+
+;;; function
+(= (funcall (function -) 3) -3)
+(= (apply #'- '(4 3)) 1)
+
+
+;;; lambda
+(= ((lambda (x y) (+ (* x x) (* y y))) 3 4) 25)
+(equal ((lambda (x y &rest z) z) 3 4 5 6) '(5 6))
+(equal ((lambda (x y :rest z) z) 3 4 5 6) '(5 6))
+(= (funcall (lambda (x y) (- y (* x y))) 7 3) -18)
+
+
+;;; labels
+(eq (labels  ((evenp (n)
+                     (if (= n 0)
+                         t
+                       (oddp (- n 1))))
+              (oddp (n)
+                    (if (= n 0)
+                        nil
+                      (evenp (- n 1)))))
+      (evenp 88))
+    t)
+
+
+;;; flet
+(= (flet ((f (x) (+ x 3)))
+     (flet ((f (x) (+ x (f x))))
+       (f 7)))
+   17)
+
+
+;;; apply
+(= (apply (if (< 1 2) (function max) (function min)) 1 2 (list 3 4)) 4)
+(defun compose (f g)
+  (lambda (:rest args)
+    (funcall f (apply g args))))
+(= (funcall (compose (function sqrt) (function *)) 12 75) 30)
+
+
+;;; funcall
+(= (let ((x '(1 2 3)))
+     (funcall (cond ((listp x) (function car))
+                    (t (lambda (x) (cons x 1))))
+              x))
+   1)
 
