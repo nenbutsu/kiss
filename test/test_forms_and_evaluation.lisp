6fc684b695f007d7c5005749fd4f2342db766654
Yuji Minejima <yuji@minejima.jp>
2018-05-24 03:46:04 +0900

    new

---
+++ test/test_forms_and_evaluation.lisp
@@ -0,0 +1,17 @@
+;;; -*- mode: lisp; coding: utf-8 -*- 
+;;; test_forms_and_evaluation.lisp --- a bunch of forms with which ISLisp processor must return true.
+
+;; Copyright (C) 2017 Yuji Minejima.
+
+;; This file is part of ISLisp processor KISS.
+
+;; KISS is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; KISS is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
