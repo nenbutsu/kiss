b73b7c44a34357fb6dbeb1441e2dbced508cbb26
Yuji Minejima <yuji@minejima.jp>
2018-05-25 14:15:28 +0900

    added tests for (setf (garef ga ...))

--- test/test_setf.lisp
+++ test/test_setf.lisp
@@ -16,8 +16,56 @@
 ;; GNU General Public License for more details.
 
 ;;; setf
+
+;; (setf (aref array ...))
 (defdynamic dynamic-a 10)
 (and (= (dynamic dynamic-a) 10)
      (setf (dynamic dynamic-a) 0)
      (= (dynamic dynamic-a) 0))
+
+(defglobal array1 (create-array '(3 3 3) 0))
+(equal array1
+       #3a(((0 0 0) (0 0 0) (0 0 0))
+           ((0 0 0) (0 0 0) (0 0 0))
+           ((0 0 0) (0 0 0) (0 0 0))))
+(setf (aref array1 0 1 2) 'x)
+(equal array1
+       #3a(((0 0 0) (0 0 x) (0 0 0))
+           ((0 0 0) (0 0 0) (0 0 0))
+           ((0 0 0) (0 0 0) (0 0 0))))
+(setf (aref array1 2 1 0) 'z)
+(equal array1
+       #3a(((0 0 0) (0 0 x) (0 0 0))
+           ((0 0 0) (0 0 0) (0 0 0))
+           ((0 0 0) (z 0 0) (0 0 0))))
+
+(defglobal vec #(0 1 2 3 4))
+(setf (aref vec 0) 'a)
+(equal vec #(a 1 2 3 4))
+
+(defglobal str (create-string 3 #\space))
+(setf (aref str 2) #\z)
+(string= str "  z")
+
+
+;; (setf (garef garray ...))
+(setq vec #(0 1 2 3 4))
+(setf (garef vec 0) 'a)
+(equal vec #(a 1 2 3 4))
+
+(setq array1 (create-array '(3 3 3) 0))
+(equal array1
+       #3a(((0 0 0) (0 0 0) (0 0 0))
+           ((0 0 0) (0 0 0) (0 0 0))
+           ((0 0 0) (0 0 0) (0 0 0))))
+(setf (garef array1 0 1 2) 'x)
+(equal array1
+       #3a(((0 0 0) (0 0 x) (0 0 0))
+           ((0 0 0) (0 0 0) (0 0 0))
+           ((0 0 0) (0 0 0) (0 0 0))))
+(setf (garef array1 2 1 0) 'z)
+(equal array1
+       #3a(((0 0 0) (0 0 x) (0 0 0))
+           ((0 0 0) (0 0 0) (0 0 0))
+           ((0 0 0) (z 0 0) (0 0 0))))
 
