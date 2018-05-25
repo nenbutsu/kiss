dbe833963abc723b90783579a96bbccbcaacfd2c
Yuji Minejima <yuji@minejima.jp>
2018-05-25 16:26:06 +0900

    added test for elt, car, cdr, property

--- test/test_setf.lisp
+++ test/test_setf.lisp
@@ -69,3 +69,49 @@
            ((0 0 0) (0 0 0) (0 0 0))
            ((0 0 0) (z 0 0) (0 0 0))))
 
+
+;; (setf (elt list ...))
+(defglobal list (create-list 4 nil))
+(setf (elt list 1) 'a)
+(equal list '(nil a nil nil))
+(setf (elt list 3) 'z)
+(equal list '(nil a nil z))
+
+(setq vec #(0 1 2 3 4))
+(setf (elt vec 0) 'a)
+(equal vec #(a 1 2 3 4))
+(setf (elt vec 3) 'z)
+(equal vec #(a 1 2 z 4))
+
+(setq str (create-string 3 #\space))
+(setf (elt str 0) #\a)
+(string= str "a  ")
+(setf (elt str 2) #\z)
+(string= str "a z")
+
+
+;; (setf (car list) ...)
+(setq list (create-list 3 t))
+(setf (car list) 'a)
+(equal list '(a t t))
+(setf (car (cdr list)) 'b)
+(equal list '(a b t))
+(setf (car (cdr (cdr list))) 'c)
+(equal list '(a b c))
+
+
+;; (setf (cdr list) ...)
+(setq list (create-list 3 t))
+(progn (setf (cdr list) nil)
+       (equal list '(t)))
+(setq list (create-list 3 t))
+(progn (setf (cdr (cdr list)) nil)
+       (equal list '(t t)))
+
+
+;; (setf (property symbol) ...)
+(defglobal foo nil)
+(setf (property 'foo 'counter) 10)
+(= (property 'foo 'counter) 10)
+(setf (property 'foo 'today) 'Monday)
+(eq (property 'foo 'today) 'Monday)
