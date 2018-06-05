1e13880122addf8bc6742b3848a9084285128071
Yuji Minejima <yuji@minejima.jp>
2018-06-05 21:09:42 +0900

    added tests for characterp

--- test/test_char.lisp
+++ test/test_char.lisp
@@ -15,3 +15,47 @@
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
+
+;;; characterp
+
+(eq (characterp #\a) 't)
+(eq (characterp #\b) 't)
+(eq (characterp #\newline) 't)
+(eq (characterp #\space) 't)
+(eq (characterp 'a) 'nil)
+(eq (characterp "a") 'nil)
+(eq (characterp #()) 'nil)
+(block top
+  (mapc (lambda (x)
+          (if (not (characterp x))
+              (return-from top nil)))
+        '(#\! #\" #\# #\$ #\% #\& #\' #\( #\) #\* #\+ #\, #\- #\. #\/
+          #\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 #\: #\; #\< #\= #\> #\? 
+          #\@ #\A #\B #\C #\D #\E #\F #\G #\H #\I #\J #\K #\L #\M #\N #\O
+          #\P #\Q #\R #\S #\T #\U #\V #\W #\X #\Y #\Z #\[ #\\ #\] #\^ #\_
+          #\` #\a #\b #\c #\d #\e #\f #\g #\h #\i #\j #\k #\l #\m #\n #\o
+          #\p #\q #\r #\s #\t #\u #\v #\w #\x #\y #\z #\{ #\| #\} #\~
+          #\space #\newline))
+  t)
+(block a
+  (with-handler (lambda (condition)
+		  (if (instancep condition (class <arity-error>))
+		      (return-from a t)
+                      (signal-condition condition nil)))
+    (characterp))
+  nil)
+(block a
+  (with-handler (lambda (condition)
+		  (if (instancep condition (class <arity-error>))
+		      (return-from a t)
+                      (signal-condition condition nil)))
+    (characterp #\a #\b))
+  nil)
+(block a
+  (with-handler (lambda (condition)
+		  (if (instancep condition (class <arity-error>))
+		      (return-from a t)
+                      (signal-condition condition nil)))
+    (characterp #\a #\b #\c))
+  nil)
+
